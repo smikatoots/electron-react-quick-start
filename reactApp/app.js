fdbfca0dc3832685c9be347fd0c13c2f5000fe61
Mika Reyes <reyes.mikaelahelene@gmail.com>
2017-08-07 11:42:57 -0700

    made the formatting code shorter

--- reactApp/app.js
+++ reactApp/app.js
@@ -33,39 +33,11 @@ class MyEditor extends React.Component {
     this.onChange = (editorState) => this.setState({editorState});
   }
 
-  _onBoldClick() {
-     this.onChange(RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        'BOLD'
-     ));
-  }
-
-  _onUnderlineClick() {
-     this.onChange(RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        'UNDERLINE'
-     ));
-  }
-
-  _onItalicClick() {
-     this.onChange(RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        'ITALIC'
-     ));
-  }
-
-  _onCodeClick() {
-     this.onChange(RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        'CODE'
-     ));
-  }
-
-  _onStrikeClick() {
-     this.onChange(RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        'STRIKETHROUGH'
-     ));
+  _onFormatClick(style) {
+      this.onChange(RichUtils.toggleInlineStyle(
+         this.state.editorState,
+         style
+      ));
   }
 
   _onColorChange(event) {
@@ -78,22 +50,22 @@ class MyEditor extends React.Component {
   }
 
    _onLeftAClick() {
-      this.onChange(RichUtils.toggleInlineStyle(
-          this.textAlignment,
+      this.onChange(RichUtils.toggleBlockType(
+          this.state.editorState,
           'left'
       ));
   }
 
    _onRightAClick() {
-      this.onChange(RichUtils.toggleInlineStyle(
-          this.textAlignment,
+      this.onChange(RichUtils.toggleBlockType(
+          this.state.editorState,
           'right'
       ));
   }
 
    _onCenterAClick() {
-      this.onChange(RichUtils.toggleInlineStyle(
-          this.textAlignment,
+      this.onChange(RichUtils.toggleBlockType(
+          this.state.editorState,
           'center'
       ));
   }
@@ -102,25 +74,25 @@ class MyEditor extends React.Component {
     return (
       <div id='content' style={{width: '480px', margin: '0 auto'}}>
         <h1>Draft.js Editor</h1>
-        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-        <button onClick={this._onItalicClick.bind(this)}>Italic</button>
-        <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
-        <button onClick={this._onCodeClick.bind(this)}>Code</button>
-        <button onClick={this._onStrikeClick.bind(this)}>Strikethrough</button>
-        <select onChange={(e) => this._onColorChange(e)}>
+        <button onClick={() => this._onFormatClick('BOLD')}>Bold</button>
+        <button onClick={() => this._onFormatClick('ITALIC')}>Italic</button>
+        <button onClick={() => this._onFormatClick('UNDERLINE')}>Underline</button>
+        <button onClick={() => this._onFormatClick('CODE')}>Code</button>
+        <button onClick={() => this._onFormatClick('STRIKETHROUGH')}>Strikethrough</button>
+        <select onChange={(event) => this._onColorChange(event)}>
             <option value="red">Red</option>
             <option value="yellow">Yellow</option>
             <option value="blue">Blue</option>
         </select>
-        {/* <button onClick={this._onLeftAClick.bind(this)}>Align Left</button>
+        <button onClick={this._onLeftAClick.bind(this)}>Align Left</button>
         <button onClick={this._onCenterAClick.bind(this)}>Align Center</button>
-        <button onClick={this._onRightAClick.bind(this)}>Align Right</button> */}
+        <button onClick={this._onRightAClick.bind(this)}>Align Right</button>
         <div className='editor' style={{border: '1px solid grey', padding: '6px'}}>
           <Editor
             customStyleMap={styleMap}
             editorState={this.state.editorState}
             onChange={this.onChange}
-            textAlignment='right'
+            textAlignment={this.state.editorState}
           />
         </div>
       </div>
