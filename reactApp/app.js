c459065dea8ea824537e9e4578f65bbaf062a00c
Mika Reyes <reyes.mikaelahelene@gmail.com>
2017-08-07 10:30:16 -0700

    added italic, underline, code

--- reactApp/app.js
+++ reactApp/app.js
@@ -22,11 +22,35 @@ class MyEditor extends React.Component {
      ));
   }
 
+  _onUnderlineClick() {
+     this.onChange(RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        'UNDERLINE'
+     ));
+  }
+
+  _onItalicClick() {
+     this.onChange(RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        'ITALIC'
+     ));
+  }
+
+  _onCodeClick() {
+     this.onChange(RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        'CODE'
+     ));
+  }
+
   render() {
     return (
       <div id='content' style={{width: '480px', margin: '0 auto'}}>
         <h1>Draft.js Editor</h1>
         <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <button onClick={this._onItalicClick.bind(this)}>Italic</button>
+        <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
+        <button onClick={this._onCodeClick.bind(this)}>Code</button>
         <div className='editor' style={{border: '1px solid grey', padding: '6px'}}>
           <Editor
             editorState={this.state.editorState}
