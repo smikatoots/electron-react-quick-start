025e75c49bf2b960ec626a634e308d69f5f866f7
Mika Reyes <reyes.mikaelahelene@gmail.com>
2017-08-07 10:25:12 -0700

    setup draft.js and added bold rich utils config

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import {Editor, EditorState, RichUtils} from 'draft-js';
 
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
@@ -7,5 +8,36 @@ var ReactDOM = require('react-dom');
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
+class MyEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  _onBoldClick() {
+     this.onChange(RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        'BOLD'
+     ));
+  }
+
+  render() {
+    return (
+      <div id='content' style={{width: '480px', margin: '0 auto'}}>
+        <h1>Draft.js Editor</h1>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <div className='editor' style={{border: '1px solid grey', padding: '6px'}}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<MyEditor />,
    document.getElementById('root'));
