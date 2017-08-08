4105c62ad213eca99cc9cfd27eb085041e2c1298
Johnathan Zhou <johnathanz@berkeley.edu>
2017-08-08 10:59:39 -0700

    working with server

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
 var mongoose = require('mongoose');
-var Users = require('./models')
-var Documents = require('./models')
+var Models = require('./models')
 var MongoStore = require('connect-mongo')(session)
 var app = express();
+var Document = Models.Documents
+var User = Models.Users
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -57,45 +59,59 @@ app.get('/', function (req, res) {
 
 app.post('/save', function(req, res) {
   console.log('hey')
-  var id = req.params.id
-  Document.findById(id, function(err, doc) {
-      if (err) {
-        console.log('error in finding doc to save', err)
-      }
-      else {
-        Users.find({username: req.user}, function(err, user) {
-          if (err) {
-            console.log('error finding user', err)
-          }
-          else {
-            if (doc.author === user._id) {
-              Document.update({_id: id}, {
-                content: this.editorState
-              }), function(err, affected, resp) {
-                console.log('Document updated and saved!', resp)
-                }
-            }
-            else if (doc.collaborators.includes(user._id)) {
-              Document.update({_id: id}, {
-                content: this.editorState
-              }), function(err, affected, resp) {
-                console.log('Document updated and saved!', resp)
-                }
-            }
-            else {
-              var collabArr = doc.collaborators.slice()
-              collabArr.push(user._id)
-              Document.update({_id: id}, {
-                content: this.editorState,
-                collaborators: collabArr
-              }), function(err, affected, resp) {
-                console.log('Document updated and saved! Collaboratoradded', resp)
-                }
-              }
-            }
-          })
-        }
-      })
+  var newDoc = new Document({
+    title: 'yo',
+    content: req.body.content,
+    collaborators: []
+  })
+  console.log('yo')
+  newDoc.save(function(err, doc) {
+    if (err) {
+      console.log('error in saving', err)
+    }
+    else {
+      console.log('success! doc saved.', doc)
+    }
+  })
+  // var id = req.params.id
+  // Documents.findById(id, function(err, doc) {
+  //     if (err) {
+  //       console.log('error in finding doc to save', err)
+  //     }
+  //     else {
+  //       Users.find({username: req.user}, function(err, user) {
+  //         if (err) {
+  //           console.log('error finding user', err)
+  //         }
+  //         else {
+  //           if (doc.author === user._id) {
+  //             Document.update({_id: id}, {
+  //               content: this.editorState
+  //             }), function(err, affected, resp) {
+  //               console.log('Document updated and saved!', resp)
+  //               }
+  //           }
+  //           else if (doc.collaborators.includes(user._id)) {
+  //             Document.update({_id: id}, {
+  //               content: this.editorState
+  //             }), function(err, affected, resp) {
+  //               console.log('Document updated and saved!', resp)
+  //               }
+  //           }
+  //           else {
+  //             var collabArr = doc.collaborators.slice()
+  //             collabArr.push(user._id)
+  //             Document.update({_id: id}, {
+  //               content: this.editorState,
+  //               collaborators: collabArr
+  //             }), function(err, affected, resp) {
+  //               console.log('Document updated and saved! Collaboratoradded', resp)
+  //               }
+  //             }
+  //           }
+  //         })
+  //       }
+  //     })
 })
 
 
